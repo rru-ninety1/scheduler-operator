a019591d58cb7fb422681d17e7dac482ee686fb2
Remo <remo.rusconi@gmail.com>
2024-07-27 11:53:08 +0000

    More test

--- internal/controller/schedulation_controller_test.go
+++ internal/controller/schedulation_controller_test.go
@@ -35,12 +35,83 @@ import (
 	crdv1alpha1 "github.com/rru-ninety1/scheduler-operator/api/v1alpha1"
 )
 
+// getSchedulationReconciler returns a SchedulationReconciler with a fake recorder
 func getSchedulationReconciler() *SchedulationReconciler {
 	return &SchedulationReconciler{
 		Client:   k8sClient,
 		Scheme:   k8sClient.Scheme(),
 		Recorder: record.NewFakeRecorder(3),
 	}
+}
+
+// getExampleDeployment returns an example Deployment
+func getExampleDeployment(deploymentName, deploymentNamespace string, deploymentReplicas int32) *appsv1.Deployment {
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deploymentName,
+			Namespace: deploymentNamespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: pointer.Int32(deploymentReplicas),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "test",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "test",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "test",
+							Image: "nginx",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return deployment
+}
+
+// getExampleDeployment returns an example StatefulSet
+func getExampleStatefullset(statefullSetName, statefullSetNamespace string, statefullSetReplicas int32) *appsv1.StatefulSet {
+	statefullset := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      statefullSetName,
+			Namespace: statefullSetNamespace,
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Replicas: pointer.Int32(statefullSetReplicas),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "test",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "test",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "test",
+							Image: "nginx",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return statefullset
 }
 
 var _ = Describe("Schedulation Controller", func() {
@@ -512,6 +583,8 @@ var _ = Describe("Schedulation Controller", func() {
 
 		const deploymentName = "test-deployment"
 		const deploymentNamespace = "default"
+		const deploymentReplicas = 1
+		const deploymentScheduledReplicas = 2
 
 		BeforeEach(func() {
 			By("Creating the deployment")
@@ -521,35 +594,7 @@ var _ = Describe("Schedulation Controller", func() {
 				Namespace: deploymentNamespace,
 			}, deployment)
 			if err != nil && errors.IsNotFound(err) {
-				deployment = &appsv1.Deployment{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      deploymentName,
-						Namespace: deploymentNamespace,
-					},
-					Spec: appsv1.DeploymentSpec{
-						Replicas: pointer.Int32(1),
-						Selector: &metav1.LabelSelector{
-							MatchLabels: map[string]string{
-								"app": "test",
-							},
-						},
-						Template: corev1.PodTemplateSpec{
-							ObjectMeta: metav1.ObjectMeta{
-								Labels: map[string]string{
-									"app": "test",
-								},
-							},
-							Spec: corev1.PodSpec{
-								Containers: []corev1.Container{
-									{
-										Name:  "test",
-										Image: "nginx",
-									},
-								},
-							},
-						},
-					},
-				}
+				deployment = getExampleDeployment(deploymentName, deploymentNamespace, deploymentReplicas)
 				Expect(k8sClient.Create(ctx, deployment)).To(Succeed())
 			}
 
@@ -570,7 +615,7 @@ var _ = Describe("Schedulation Controller", func() {
 						Resources: []crdv1alpha1.ScheduledResource{
 							{
 								Type:         crdv1alpha1.ResourceTypeDeployment,
-								ReplicaCount: 2,
+								ReplicaCount: deploymentScheduledReplicas,
 								Namespace:    deploymentNamespace,
 								Name:         deploymentName,
 								Order:        1,
@@ -608,10 +653,136 @@ var _ = Describe("Schedulation Controller", func() {
 				Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
 			}
 		})
+
+		It("should execute the schedulation and set the desidered deployment replica", func() {
+			// Reconcile the Schedulation
+			By("Reconciling the created resource")
+			controllerReconciler := getSchedulationReconciler()
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			// Check if the deployment has been updated
+			By("Checking if the deployment has been updated")
+			deployment := &appsv1.Deployment{}
+			err = k8sClient.Get(ctx, types.NamespacedName{
+				Name:      deploymentName,
+				Namespace: deploymentNamespace,
+			}, deployment)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(*deployment.Spec.Replicas).To(Equal(int32(deploymentScheduledReplicas)))
+		})
 	})
-	//TODO test per deployment
-	//TODO test per statefulset
-	//TODO test con più risorse
+
+	When("Reconciling a schedulation with statefullset in execution time", func() {
+		const resourceName = "test-resource"
+		const resourceNamespace = "default"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: resourceNamespace,
+		}
+
+		const statefullSetName = "test-statefull"
+		const statefullSetNamespace = "default"
+		const statefullSetReplicas = 1
+		const statefullsetScheduledReplicas = 2
+
+		BeforeEach(func() {
+			By("Creating the statefullset")
+
+			statefullSet := &appsv1.StatefulSet{}
+			err := k8sClient.Get(ctx, types.NamespacedName{
+				Name:      statefullSetName,
+				Namespace: statefullSetNamespace,
+			}, statefullSet)
+
+			if err != nil && errors.IsNotFound(err) {
+				statefullSet = getExampleStatefullset(statefullSetName, statefullSetNamespace, statefullSetReplicas)
+				Expect(k8sClient.Create(ctx, statefullSet)).To(Succeed())
+			}
+
+			By("Creating the custom resource for the Kind Schedulation")
+			schedulation := &crdv1alpha1.Schedulation{}
+			err = k8sClient.Get(ctx, typeNamespacedName, schedulation)
+			if err != nil && errors.IsNotFound(err) {
+				schedulation = &crdv1alpha1.Schedulation{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					Spec: crdv1alpha1.SchedulationSpec{
+						Suspended: false,
+						OneShot:   true,
+						StartHour: 0,
+						EndHour:   23,
+						Resources: []crdv1alpha1.ScheduledResource{
+							{
+								Type:         crdv1alpha1.ResourceTypeStatefulSet,
+								ReplicaCount: statefullsetScheduledReplicas,
+								Namespace:    statefullSetNamespace,
+								Name:         statefullSetName,
+								Order:        1,
+							}},
+					},
+				}
+				Expect(k8sClient.Create(ctx, schedulation)).To(Succeed())
+			}
+
+			By("Set set default conditions")
+			schedulation.Status.SetDefaultConditionsIfNotSet()
+			Expect(k8sClient.Status().Update(ctx, schedulation)).To(Succeed())
+		})
+
+		AfterEach(func() {
+			// Delete the statefullset
+			By("Deleting the statefullset")
+			statefullset := &appsv1.StatefulSet{}
+
+			err := k8sClient.Get(ctx, types.NamespacedName{
+				Name:      statefullSetName,
+				Namespace: statefullSetNamespace,
+			}, statefullset)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(k8sClient.Delete(ctx, statefullset)).To(Succeed())
+
+			// Delete the schedulation
+			resource := &crdv1alpha1.Schedulation{}
+			err = k8sClient.Get(ctx, typeNamespacedName, resource)
+
+			if !errors.IsNotFound(err) {
+				Expect(err).NotTo(HaveOccurred())
+
+				By("Cleanup the specific resource instance Schedulation")
+				Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+			}
+		})
+
+		It("should execute the schedulation and set the desidered statefullset replica", func() {
+			// Reconcile the Schedulation
+			By("Reconciling the created resource")
+			controllerReconciler := getSchedulationReconciler()
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			// Check if the statefullset has been updated
+			By("Checking if the statefullset has been updated")
+			statefullset := &appsv1.StatefulSet{}
+			err = k8sClient.Get(ctx, types.NamespacedName{
+				Name:      statefullSetName,
+				Namespace: statefullSetNamespace,
+			}, statefullset)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(*statefullset.Spec.Replicas).To(Equal(int32(statefullsetScheduledReplicas)))
+		})
+	})
 
 	When("Reconciling a one-shot schedulation in execution time", func() {
 		const resourceName = "test-resource"
